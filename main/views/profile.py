0e34f2c73bec18a0e06363b970321f2bc801c7cc
Kllaudiia <klaudi909@gmail.com>
2023-06-19 00:56:04 +0200

    profile - services (password_change)

--- main/views/profile.py
+++ main/views/profile.py
@@ -3,9 +3,9 @@
 from django.shortcuts import render, redirect
 import datetime
 from ..forms import EditUserForm
-from ..services.profil import profil_save_edit
+from ..services.profil import profil_save_edit, profil_save_password_change
 from django.contrib import messages
-from django.contrib.auth import update_session_auth_hash
+
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
@@ -21,20 +21,7 @@ def profile(request):
 
     if request.method == "POST":       
         if "save_password_change" in request.POST:
-            old_password = request.POST['old_password']
-            if request.user.check_password(old_password):
-
-                new_password1 = request.POST['password1']
-                new_password2 = request.POST['password2']
-                if new_password1 == new_password2:
-                    request.user.set_password(new_password1)
-                    request.user.save()
-                    update_session_auth_hash(request, request.user)
-                    messages.success(request, 'Twoje hasło zostało zmienione.')
-                    return redirect('profile')
-                else:
-                    messages.error(request, 'Podane hasła nie są identyczne.')
-                    return redirect('profile')
+            profil_save_password_change(request)
 
 
         elif "save_edit" in request.POST:
