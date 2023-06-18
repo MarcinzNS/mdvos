4a9e7c64f96661a38a3913dab278235647bb7c20
MarcinzNS <marcin.kondeusz@gmail.com>
2023-06-19 00:00:23 +0200

    Poprawki

--- main/views/categories.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import datetime
-
-# Create your views here.
-def categories(request):
-    context = {
-        "now" : datetime.datetime.now()
-    }
-    return render(request, "index.html", context)
